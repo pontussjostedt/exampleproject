b88c84774d70f5aaafac2b49b48d0a53f0c0988d
Pontus Sjostedt <po7853sj-s@student.lu.se>
2024-01-23 20:27:44 +0100

    show added input

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	let inputContent = '';
 	let tasks: string[] = [];
+
 	const onclick = () => {
 		//called everytime we press the add button
-		tasks.push(inputContent);
-		console.log(tasks);
+		tasks = [...tasks, inputContent]; // need to reasign tasks for svelte to update foreach block
 	};
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center">
+<div class="flex h-screen flex-col items-center justify-items-center">
 	<p>text</p>
 	<div class="grid-rows-2">
 		<input class="border-2 border-pink-400" bind:value={inputContent} />
 		<button class="bg-pink-400" on:click={onclick}>Add</button>
+	</div>
+	<div class="flex flex-col">
+		{#each tasks as task}
+			<h1>{task}</h1>
+		{/each}
 	</div>
 </div>
 
