860e646064fc03a4f5a1de47bee08d9655d41809
Pontus Sjöstedt <po7853sj-s@student.lu.se>
2024-01-24 09:56:12 +0100

    add refresh buttn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,16 +2,23 @@
 	import config from "@/public-config"
 	import { onMount } from "svelte";
 	import type { PageData } from "./$types";
+	import { invalidate } from "$app/navigation";
 
 	let inputContent = '';
 	let tasks: string[] = [];
 
-	const onclick = async () => {
+	const onClick = async () => {
 		if(inputContent.length > 0){
 			await fetch(`${config.API_URL}/addmessage?message=${inputContent}`, {headers: {Origin: "http://localhost:3000"}}) //bad practice to hardcode in url string
-			tasks = [...tasks, inputContent]; // need to reasign tasks for svelte to update foreach block
+			tasks = [...tasks, inputContent]; // svelte rerenders on assignment
 		}
 	};
+
+	const onRefreshClick = async () => {
+		await invalidate(`${config.API_URL}/messages/`) // makes svelte reloade the page data (it is requested in +page.ts)
+		tasks = data.messages 
+
+	}
 
 	export let data: PageData
 
@@ -24,7 +31,8 @@
 	<p>text</p>
 	<div class="grid-rows-2">
 		<input class="border-2 border-pink-400" bind:value={inputContent} />
-		<button class="bg-pink-400" on:click={onclick}>Add</button>
+		<button class="bg-pink-400" on:click={onClick}>Add</button>
+		<button class="bg-pink-400" on:click={onRefreshClick}>Refresh</button>
 	</div>
 	<div class="flex flex-col">
 		{#each tasks as task}
--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,6 +1,6 @@
 import config from '@/public-config';
 import type { PageLoad } from './$types';
-export const load: PageLoad = async ({fetch}) => {
+export const load: PageLoad = async ({fetch, depends}) => {
     console.log("SEND REQUEST")
     const res = await fetch(`${config.API_URL}/messages/`, {
         mode: "cors",
@@ -9,6 +9,9 @@ export const load: PageLoad = async ({fetch}) => {
         }
         
     })
+
+    depends("app:data") // https://kit.svelte.dev/docs/load
+
     const messages: string[] = await res.json() ?? []
 
     return {
