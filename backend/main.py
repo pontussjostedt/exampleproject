91f4754003437254b57747da86dceee05a831c52
Pontus Sjöstedt <po7853sj-s@student.lu.se>
2024-01-24 08:10:49 +0100

    Add route backend

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/messages")
+async def get_message() -> list[str]:
+    return ["Bob", "Foo", "Bar"]
